d9032c222675eb0432b8ae5f63ae9e89ea8f1fd9
Pedro Bohlmann <pedro.bohlmann@sap.com>
2021-10-27 10:27:51 -0300

    Adding MFCC for current audio

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import matplotlib.pyplot as plt
 from python_speech_features import mfcc
-import scipy.io.wavfile as wav
+import sounddevice as sd
+from scipy.io.wavfile import write
 
-(sample_rate, sig) = wav.read("StarWars3.wav")
-
-mfcc_feat = mfcc(sig, sample_rate)
-mfcc_data = mfcc_feat.T
-
+fs = 44100  # Sample rate
+seconds = 5  # Duration of recording
+sd.default.device = 'Loopback Audio 2, Core Audio'
 plt.figure(figsize=(25, 8))
-plt.imshow(mfcc_data, cmap=plt.cm.jet, aspect='auto', origin='lower')
-plt.title('Star Wars MFCC', fontsize=18)
+plt.title('Current audio MFCC', fontsize=18)
 plt.xlabel('Time [s]', fontsize=18)
 plt.ylabel('MFCC', fontsize=18)
-plt.show()
+imageCount = 0
+imageName = 'mfcc-{}'
+
+while True:
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
+    mfcc_feat = mfcc(myrecording, fs)
+    mfcc_data = mfcc_feat.T
+    plt.imshow(mfcc_data, cmap=plt.cm.jet, aspect='auto', origin='lower')
+    # plt.show()
+    plt.savefig(imageName.format(imageCount))
+    write(imageName.format(imageCount)+'.wav',fs,myrecording)
+    imageCount+=1
